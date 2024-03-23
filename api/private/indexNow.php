4637bbed8baf339ead1de7fd3a8bade02e39046d
Svgta <svgta@live.com>
2024-03-23 15:40:37 +0100

    update

--- api/private/indexNow.php
+++ api/private/indexNow.php
@@ -23,12 +23,91 @@ class indexNow
       $lastUpdateF = $_ENV['INDEX_NOW_DATAPATH'] . '/' . self::$lastUpdateF;
       if(!is_file($lastUpdateF))
         file_put_contents($lastUpdateF, json_encode([
-          'lastUpdate' => time() - self::$delay * 24 * 60 * 60
+          'lastUpdate' => time() - self::$delay * 24 * 60 * 60,
+          'lastDelete' => 0
         ]));
 
       $lU = json_decode(file_get_contents($lastUpdateF));
       $this->lastUpdate = $lU->lastUpdate;
+      if(!isset($lU->lastDelete))
+        $this->lastDelete = 0;
+      else
+        $this->lastDelete = $lU->lastDelete;
     }
+  }
+  public function delete(): array
+  {
+    if(!isset($_ENV['INDEX_NOW']))
+      return ['INDEX_NOW' => 'No parameters'];
+    if($_ENV['INDEX_NOW'] == 0)
+      return ['INDEX_NOW' => 'Not activated'];
+    $list = $this->toDelete();
+    if(is_null($list))
+      return ['INDEX_NOW' => 'No List'];
+    return $list;
+  }
+  private function toDelete(): ?array
+  {
+    $res = [];
+    $arts = $this->artToDelete();
+    /*foreach($arts as $art)
+      $res[] = $art;*/
+    $menus = $this->menuToDelete();
+    foreach($menus as $menu)
+      $res[] = $menu;
+    if(count($res) == 0)
+      return null;
+    return $res;
+  }
+  private function menuToDelete()
+  {
+    $res = [];
+    $menuC = $this->dbRes['class']::get(
+      col: 'menus',
+      param: [
+        'deleted' => true,
+        'dateUpdate' => ['$gte' => $this->lastDelete]
+      ],
+      projection: ['name']
+    );
+    foreach($menuC as $menu){
+      $res[] = 'https://' . $_ENV['DOMAIN'] . '/' . seo::seofy($menu->name);
+    }
+    if(count($res) === 0)
+      return null;
+    return $res;
+  }
+  private function artToDelete()
+  {
+    $res = [];
+    $artC = $this->dbRes['class']::get(
+      col: 'articles',
+      param: [
+        'deleted' => true,
+        'dateUpdate' => ['$gte' => $this->lastDelete]
+      ],
+      projection: ['title', 'uuid', 'parent']
+    );
+    foreach($artC as $art){
+      if(!isset(self::$menus[$art->parent])){
+        $menu = self::$menus[$art->parent] = $this->dbRes['class']::getOne(
+          col: "menus",
+          param: ['uuid' => $art->parent],
+          projection: ['name', 'uuid']
+        );
+        if(is_null($menu))
+          continue;
+        self::$menus[$art->parent] = $menu;
+      }
+      $menu = self::$menus[$art->parent];
+      if($art->title === "")
+        $res[] = 'https://' . $_ENV['DOMAIN'] . '/' . seo::seofy($menu->name);
+      else
+        $res[] = 'https://' . $_ENV['DOMAIN'] . '/' . seo::seofy($menu->name) . '/' . seo::seofy($art->title);
+    }
+    if(count($res) === 0)
+      return null;
+    return $res;
   }
   public function index(): array
   {
@@ -103,5 +182,9 @@ class indexNow
 }
 
 $index = new indexNow();
-$ret = $index->index();
+
+$ret = [
+  'toIndex' => $index->index(),
+  'toDelete' => $index->delete()
+];
 echo json_encode($ret, JSON_PRETTY_PRINT);
