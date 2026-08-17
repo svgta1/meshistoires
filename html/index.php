9ea37a5626be25714f09d1abbc48a38b2bcc987d
Svgta <svgta@sd-123477.dedibox.fr>
2026-08-17 19:26:44 +0200

    optimisation

--- html/index.php
+++ html/index.php
@@ -56,7 +56,7 @@ class setIndex
   public function setContents()
   {
     $contents = opt::file_get_contents('index.html.template');
-    $contents = str_replace('##image##', $this->aff['image'], $contents);
+    $contents = str_replace('##image##', str_replace('/imageThumb300/', '/image/', $this->aff['image']), $contents);
     $contents = str_replace('##title##', $this->aff['title'], $contents);
     $contents = str_replace('##description##', $this->aff['description'], $contents);
     $contents = str_replace('##siteTitle##', $_ENV['SITE_TITLE'], $contents);
