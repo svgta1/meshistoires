9c8528f9b5d5ccf7b2439963492bb6f9c533e240
Svgta <svgta@sd-123477.dedibox.fr>
2026-08-18 18:14:15 +0200

    delete index

--- api/cli/indexGoogle_deleteFromFile.php
+++ api/cli/indexGoogle_deleteFromFile.php
@@ -1,20 +1,25 @@
 <?php
-use GuzzleHttp\Client;
+use Google\Client;
 
 require dirname(__FILE__, 2) . '/vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable(dirname(__FILE__, 2));
 $dotenv->load();
 
-class indexNow
+class indexGoogle
 {
   private static $lastDeleteF = 'lastDelete.json';
   private static $fileName = '';
 
   public function __construct($filename)
   {
-    $this->client = new Client();
-    if(isset($_ENV['INDEX_NOW'])){
-      $lastDeleteF = $_ENV['INDEX_NOW_DATAPATH'] . '/' . self::$lastDeleteF;
+    if(isset($_ENV['INDEX_GOOGLE'])){
+      $client = new Client();
+      $client->setAuthConfig($_ENV['INDEX_GOOGLE_KEY']);
+      $client->addScope('https://www.googleapis.com/auth/indexing');
+      $this->endpoint = 'https://indexing.googleapis.com/v3/urlNotifications:publish';
+      $this->auth = $client->authorize();
+
+      $lastDeleteF = $_ENV['INDEX_GOOGLE_DATAPATH'] . '/' . self::$lastDeleteF;
       if(!is_file($lastDeleteF))
         file_put_contents($lastDeleteF, json_encode([
           'lastDelete' => 0
@@ -22,32 +27,28 @@ class indexNow
 
       $lD = json_decode(file_get_contents($lastDeleteF));
       $this->lastDelete = $lD->lastDelete;
-     self::$fileName = $_ENV['INDEX_NOW_DATAPATH'] . '/' . $filename;
+      self::$fileName = $_ENV['INDEX_GOOGLE_DATAPATH'] . '/' . $filename;
     }
   }
   public function delete(): array
   {
     $list = $this->_getList();
     if(is_null($list))
-      return ['INDEX_NOW' => 'No List'];
+      return ['INDEX_GOOGLE' => 'No List'];
 
+    foreach($list['list'] as $url){
+      $content = json_encode([
+        'url' => $url,
+        'type' => 'URL_DELETED'
+      ]);
+      $response = $this->auth->post($this->endpoint, [ 'body' => $content ]);
+      $status_code = $response->getStatusCode();
+      print_r($response->getBody()->getContents());
+      print_r(PHP_EOL);
+      if($status_code >= 300)
+        return ['error' => $response->getStatusCode()];
+    }
 
-    $params = [
-      'host' => $_ENV['DOMAIN'],
-      'key' => $_ENV['INDEX_NOW_KEY'],
-      'keyLocation' => 'https://' . $_ENV['DOMAIN'] . '/' . $_ENV['INDEX_NOW_KEY'] . '.txt',
-      'urlList' => $list['list'],
-    ];
-
-    //print_r($params);die();
-    $resp = $this->client->post($_ENV['INDEX_NOW_API'],[
-      'json' => $params,
-      'debug' => true
-    ]);
-
-    $ret = $resp->getBody()->getContents();
-    print_r($ret);
-    print_r(PHP_EOL);
     $lastDeleteF = $_ENV['INDEX_NOW_DATAPATH'] . '/' . self::$lastDeleteF;
     file_put_contents($lastDeleteF, json_encode([
       'lastDelete' => time()
@@ -79,7 +80,7 @@ class indexNow
 }
 
 
-$index = new indexNow('delete.csv');
+$index = new indexGoogle('delete.csv');
 
 $ret = [
   'toDelete' => $index->delete()
