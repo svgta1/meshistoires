aac35ce5945fc04da32bd2f3f8522b6d17be3ca1
Svgta1 <svgta@sd-123477.dedibox.fr>
2026-08-22 17:04:41 +0200

    secu

--- api/lib/utils/security.php
+++ api/lib/utils/security.php
@@ -11,15 +11,50 @@ class security
     $rules = opt::yaml_parse_file($_ENV['SECURITY_YAML'])['rules'];
     $sure = true;
     foreach($rules as $k => $v){
-      if(strpos($query, $k) !== false){
+      if(str_contains($query, $k) !== false){
         $sure = false;
         break;
       }
-      if(strpos(urldecode($query), $k) !== false){
+      if(str_contains(strtolower($query), $k) !== false){
+        $sure = false;
+        break;
+      }
+      if(str_contains(urldecode($query), $k) !== false){
+        $sure = false;
+        break;
+      }
+      if(str_contains(self::unicodeConv($query), $k) !== false){
+        $sure = false;
+        break;
+      }
+      if(str_contains(self::hex2bin($query), $k) !== false){
+        $sure = false;
+        break;
+      }
+      if(str_contains(html_entity_decode($query), $k) !== false){
         $sure = false;
         break;
       }
     }
     return $sure;
+  }
+  public static function unicodeConv($unicode)
+  {
+    return preg_replace_callback('/\\\\u([0-9a-fA-F]{4})/', function($m) {
+        return chr(hexdec($m[1]));
+    }, $unicode);
+  }
+  public static function hex2bin($hex)
+  {
+    $hex = preg_replace('/^0x/', '', $hex);
+    $hex = preg_replace('/--$/', '', $hex);
+    try{
+      $ret = @hex2bin($hex);
+      if($ret)
+        return $ret;
+      return $hex;
+    }catch(Throwable $t){
+      return $hex;
+    }
   }
 }
